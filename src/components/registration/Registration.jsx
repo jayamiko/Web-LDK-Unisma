29921dcf7acf743a839f6fd53a422ee6f1d27d19
jayamiko <jayamiko4@gmail.com>
2022-07-24 19:09:41 +0700

    Fixed Latout Form Registration and About Card

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -98,8 +98,7 @@ function RegistrationPage() {
   
   return (
     <>
-      {/* <Header /> */}
-      <section className='heroRegistration -z-10'>
+      <section className='heroRegistration z-10'>
         <div className="container mx-auto flex flex-col md:justify-center h-full mt-32 md:mt-0 text-white px-6 py-8 md:p-0">
           <form name="submit-to-google-sheet" className='sm:w-2/3 md:w-3/6 lg:w-2/5 xl:w-2/6 space-y-1' method="post" ref={formRef} onSubmit={handleSubmit}>
             <h2 className='font-titan text-2xl mb-1 tracking-wide'>Bergabung Bersama Kami!</h2>
@@ -123,32 +122,32 @@ function RegistrationPage() {
             </div>
             <div>
               <label htmlFor="fakultas">Fakultas</label>
-              <div className='flex space-x-3'>
-                <div>
+              <div className='flex flex-wrap w-full md:w-5/6'>
+                <div className='mr-5'>
                   <input type="radio" id="fakultas" name="fakultas" value="FAI" onChange={handleChange} checked/>
                   <label className="ml-1" htmlFor="FAI">FAI</label>
                 </div>
-                <div>
+                <div className='mr-5'>
                   <input type="radio" id="fakultas" name="fakultas" value="FISIP" onChange={handleChange}/>
                   <label className="ml-1" htmlFor="FISIP">FISIP</label>
                 </div>
-                <div>
+                <div className='mr-5'>
                   <input type="radio" id="fakultas" name="fakultas" value="FAPERTA" onChange={handleChange}/>
                   <label className="ml-1" htmlFor="FAPERTA">FAPERTA</label>
                 </div>
-                <div>
+                <div className='mr-5'>
                   <input type="radio" id="fakultas" name="fakultas" value="FKIP" onChange={handleChange}/>
                   <label className="ml-1" htmlFor="FKIP">FKIP</label>
                 </div>
-                <div>
+                <div className='mr-5'>
                   <input type="radio" id="fakultas" name="fakultas" value="FATEK" onChange={handleChange}/>
                   <label className="ml-1" htmlFor="FATEK">FATEK</label>
                 </div>
-                <div>
+                <div className='mr-5'>
                   <input type="radio" id="fakultas" name="fakultas" value="FE" onChange={handleChange}/>
                   <label className="ml-1" htmlFor="FE">FE</label>
                 </div>
-                <div>
+                <div className='mr-5'>
                   <input type="radio" id="fakultas" name="fakultas" value="FKSB" onChange={handleChange}/>
                   <label className="ml-1" htmlFor="FKSB">FKSB</label>
                 </div>
@@ -162,7 +161,7 @@ function RegistrationPage() {
               <label htmlFor="jurusan">Jurusan</label>
               <input type="text" className="form-control" value={personalMember.study} id="jurusan" name="jurusan" placeholder="Jurusan" onChange={handleChange} />
             </div>
-            <button type="submit" className="btn bg-[#1eb2a6] my-4 text-white px-5 uppercase">Submit</button>
+            <button type="submit" className="btn bg-[#1eb2a6] my-4 text-white px-5 uppercase hover:font-bold">Submit</button>
           </form>
         </div>
       </section>
