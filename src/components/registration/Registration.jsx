a87519b56c70871b6c32f35c54f27a7643d32090
jayamiko <jayamiko4@gmail.com>
2022-07-24 16:51:38 +0700

    Fix style Form Registration Mobile & add Fakultas list(Test)

--- src/components/registration/Registration.jsx
+++ src/components/registration/Registration.jsx
@@ -98,58 +98,76 @@ function RegistrationPage() {
   
   return (
     <>
-      <Hero bgHero='heroRegistration' />
-      <div className="container absolute h-screen lg:mx-32 my-2 text-white px-6 py-8 md:p-0">
-        <h2 className='font-titan text-2xl mt-10 mb-1 tracking-wide'>Bergabung Bersama Kami!</h2>
-        <form name="submit-to-google-sheet" className='absolute sm:w-2/3 md:w-3/6 lg:w-2/5 xl:w-2/6 space-y-1 md:mr-4' method="post" ref={formRef} onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="fullName">Nama Lengkap</label>
-            <input type="text" className="form-control" value={personalMember.fullName} id="fullName" name="fullName" placeholder="Nama Lengkap" onChange={handleChange}/>
-          </div>
-          <div>
-            <label htmlFor="email">Alamat Email</label>
-            <input type="email" className="form-control" value={personalMember.email} id="email" name="email" placeholder="Enter email" onChange={handleChange}/>
-          </div>
-          <div className="flex justify-between">
-            <div className='w-2/5'>
-              <label htmlFor="usia">Usia</label>
-              <input type="text" className="form-control" value={personalMember.age} id="usia" name="usia" placeholder="cth : 19" onChange={handleChange}/>
+      {/* <Header /> */}
+      <section className='heroRegistration -z-10'>
+        <div className="container mx-auto flex flex-col md:justify-center h-full mt-32 md:mt-0 text-white px-6 py-8 md:p-0">
+          <form name="submit-to-google-sheet" className='sm:w-2/3 md:w-3/6 lg:w-2/5 xl:w-2/6 space-y-1' method="post" ref={formRef} onSubmit={handleSubmit}>
+            <h2 className='font-titan text-2xl mb-1 tracking-wide'>Bergabung Bersama Kami!</h2>
+            <div>
+              <label htmlFor="fullName">Nama Lengkap</label>
+              <input type="text" className="form-control" value={personalMember.fullName} id="fullName" name="fullName" placeholder="Nama Lengkap" onChange={handleChange}/>
             </div>
-            <div className='w-3/6'>
-              <label htmlFor="whatsapp">Whatsapp</label>
-              <input type="text" className="form-control" value={personalMember.whatsapp} id="whatsapp" name="whatsapp" placeholder="08xxxxxxxx" onChange={handleChange}/>
+            <div>
+              <label htmlFor="email">Alamat Email</label>
+              <input type="email" className="form-control" value={personalMember.email} id="email" name="email" placeholder="Enter email" onChange={handleChange}/>
             </div>
-          </div>
-          <div>
-            <label htmlFor="fakultas">Fakultas</label>
-            <div className='flex space-x-3'>
-              <div>
-                <input type="radio" id="fakultas" name="fakultas" value="FAI" onChange={handleChange} checked/>
-                <label className="ml-1" htmlFor="FAI">FAI</label>
+            <div className="flex justify-between">
+              <div className='w-2/5'>
+                <label htmlFor="usia">Usia</label>
+                <input type="text" className="form-control" value={personalMember.age} id="usia" name="usia" placeholder="cth : 19" onChange={handleChange}/>
               </div>
-              <div>
-                <input type="radio" id="fakultas" name="fakultas" value="FISIP" onChange={handleChange}/>
-                <label className="ml-1" htmlFor="FISIP">FISIP</label>
-              </div>
-              <div>
-                <input type="radio" id="fakultas" name="fakultas" value="FKIP" onChange={handleChange}/>
-                <label className="ml-1" htmlFor="FKIP">FKIP</label>
+              <div className='w-3/6'>
+                <label htmlFor="whatsapp">Whatsapp</label>
+                <input type="text" className="form-control" value={personalMember.whatsapp} id="whatsapp" name="whatsapp" placeholder="08xxxxxxxx" onChange={handleChange}/>
               </div>
             </div>
-          </div>
-          <div>
-            <label htmlFor="angkatan">Angkatan</label>
-            <input type="text" className="form-control" value={personalMember.study} id="angkatan" name="angkatan" placeholder="cth: 2019" onChange={handleChange} />
-          </div>
-          <div>
-            <label htmlFor="jurusan">Jurusan</label>
-            <input type="text" className="form-control" value={personalMember.study} id="jurusan" name="jurusan" placeholder="Jurusan" onChange={handleChange} />
-          </div>
-          <button type="submit" className="btn bg-[#1eb2a6] my-2 text-white px-5 uppercase">Submit</button>
-        </form>
-      </div>
-      <div className='h-screen lg:h-[80vh]'></div>
-      <div className='container mx-auto' style={{ height: 630, width: '100%' }}>
+            <div>
+              <label htmlFor="fakultas">Fakultas</label>
+              <div className='flex space-x-3'>
+                <div>
+                  <input type="radio" id="fakultas" name="fakultas" value="FAI" onChange={handleChange} checked/>
+                  <label className="ml-1" htmlFor="FAI">FAI</label>
+                </div>
+                <div>
+                  <input type="radio" id="fakultas" name="fakultas" value="FISIP" onChange={handleChange}/>
+                  <label className="ml-1" htmlFor="FISIP">FISIP</label>
+                </div>
+                <div>
+                  <input type="radio" id="fakultas" name="fakultas" value="FAPERTA" onChange={handleChange}/>
+                  <label className="ml-1" htmlFor="FAPERTA">FAPERTA</label>
+                </div>
+                <div>
+                  <input type="radio" id="fakultas" name="fakultas" value="FKIP" onChange={handleChange}/>
+                  <label className="ml-1" htmlFor="FKIP">FKIP</label>
+                </div>
+                <div>
+                  <input type="radio" id="fakultas" name="fakultas" value="FATEK" onChange={handleChange}/>
+                  <label className="ml-1" htmlFor="FATEK">FATEK</label>
+                </div>
+                <div>
+                  <input type="radio" id="fakultas" name="fakultas" value="FE" onChange={handleChange}/>
+                  <label className="ml-1" htmlFor="FE">FE</label>
+                </div>
+                <div>
+                  <input type="radio" id="fakultas" name="fakultas" value="FKSB" onChange={handleChange}/>
+                  <label className="ml-1" htmlFor="FKSB">FKSB</label>
+                </div>
+              </div>
+            </div>
+            <div>
+              <label htmlFor="angkatan">Angkatan</label>
+              <input type="text" className="form-control" value={personalMember.study} id="angkatan" name="angkatan" placeholder="cth: 2019" onChange={handleChange} />
+            </div>
+            <div>
+              <label htmlFor="jurusan">Jurusan</label>
+              <input type="text" className="form-control" value={personalMember.study} id="jurusan" name="jurusan" placeholder="Jurusan" onChange={handleChange} />
+            </div>
+            <button type="submit" className="btn bg-[#1eb2a6] my-4 text-white px-5 uppercase">Submit</button>
+          </form>
+        </div>
+      </section>
+      <div className='h-screen'></div>
+      <div className='container mx-auto mt-32 md:mt-0' style={{ height: 630, width: '100%' }}>
         <DataGrid
           rows={dataRow}
           columns={columns}
